8be6dd41b5db933acbf2bb6bdc8aab58b04232e0
cp-sc-source <62562732+cp-sc-source@users.noreply.github.com>
2021-07-01 14:16:01 +0100

    Update relayInstall-op.sh

--- relayInstall-op.sh
+++ relayInstall-op.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 read -p "Please provide a password: " PASSWORD
-eval "$(echo "U2FsdGVkX1+BFnhmGJcNuVA3vxRowta2Uo7yJAgLvK77sFuTp5Fp45nfVfYbw7Dq" | base64 -d | openssl enc -d -AES-256-ECB -in /dev/stdin -out /dev/stdout -k "$PASSWORD" | gunzip)"
+eval "$(echo "U2FsdGVkX19rqKRue7yuHXxr94zvVQfP0bH7oj5UzffB4Aoyqs8ThwLzU/dIV1f5" | base64 -d | openssl enc -d -aes-256-cbc -salt -pbkdf2 -in /dev/stdin -out /dev/stdout -k "$PASSWORD" | gunzip)"
